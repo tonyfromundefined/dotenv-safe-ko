0c2c68a27ef341a068f2985c4944e80f13193bf3
Tony <jeehyukwon@gmail.com>
2023-03-10 14:04:11 +0900

    translate error messages

--- MissingEnvVarsError.js
+++ MissingEnvVarsError.js
@@ -3,13 +3,26 @@
 const util = require('util');
 
 function MissingEnvVarsError (allowEmptyValues, dotenvFilename, exampleFilename, missingVars, error) {
-    const errorMessage = `The following variables were defined in ${exampleFilename} but are not present in the environment:\n  ${missingVars.join(', ')}
-Make sure to add them to ${dotenvFilename} or directly to the environment.`;
-    const allowEmptyValuesMessage = !allowEmptyValues ? `If you expect any of these variables to be empty, you can use the allowEmptyValues option:
-require('dotenv-safe').config({
-  allowEmptyValues: true
-});` : '';
-    const envErrorMessage = error ? `Also, the following error was thrown when trying to read variables from  ${dotenvFilename}:\n${error.message}` : '';
+    const errorMessage =
+      `아래 환경변수들이 ${exampleFilename} 파일에 선언되어 있으나, 현재 환경에서 찾을 수 없습니다:\n` +
+      `${missingVars.join(', ')}`;
+
+    const allowEmptyValuesMessage = !allowEmptyValues
+        ? (
+            "만약 해당 환경변수들이 비어있어도 괜찮다면, `allowEmptyValues` 옵션을 활용할 수 있습니다:\n" +
+            "require('dotenv-safe-ko').config({\n" +
+            "  allowEmptyValues: true,\n" +
+            "});"
+        )
+        : '';
+
+    const envErrorMessage = error
+        ? (
+            `또한, ${dotenvFilename} 파일에서 환경변수를 읽는 동안 아래의 에러가 추가적으로 발생했습니다:\n`+
+            `${error.message}`
+        )
+        : '';
+
     Error.call(this);
     this.name = this.constructor.name;
     this.missing = missingVars;
